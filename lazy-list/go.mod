22f9e24ac5498c7d334bd5d5a71ba679b2940c87
MotNosniktaw <tomwatkinsondeveloper@gmail.com>
2023-08-31 19:23:25 +0100

    first commit

---
+++ lazy-list/go.mod
@@ -0,0 +1,5 @@
+module github.com/motnosniktaw/htmx-lazy-list
+
+go 1.20
+
+require github.com/go-chi/chi v1.5.4 // indirect
---
+++ lazy-list/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/go-chi/chi"
+)
+
+type Todo struct {
+	Id    int
+	Title string
+	Done  bool
+}
+
+type TodoTableData struct {
+	Todos []Todo
+}
+
+type TodoPageData struct {
+	PageTitle string
+}
+
+func main() {
+	router := chi.NewRouter()
+
+	pageTmpl := template.Must(template.ParseFiles("page.html"))
+	tableTmpl := template.Must(template.ParseFiles("table.html"))
+	rowTmpl := template.Must(template.ParseFiles("row.html"))
+
+	tableData := TodoTableData{
+		Todos: []Todo{
+			{
+				Id:    1,
+				Title: "Thing",
+				Done:  true,
+			},
+			{
+				Id:    2,
+				Title: "Theng",
+				Done:  true,
+			},
+			{
+				Id:    3,
+				Title: "Thung",
+				Done:  false,
+			},
+			{
+				Id:    4,
+				Title: "Thang",
+				Done:  false,
+			},
+		},
+	}
+
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		pageData := TodoPageData{
+			PageTitle: "TODO - HTMX Lazy List",
+		}
+
+		pageTmpl.Execute(w, pageData)
+	})
+
+	router.Get("/todos", func(w http.ResponseWriter, r *http.Request) {
+		rand.Seed(time.Now().UnixNano())
+		n := 100 + rand.Intn(400)
+		time.Sleep(time.Duration(n) * time.Millisecond)
+
+		tableTmpl.Execute(w, tableData)
+	})
+
+	router.Get("/todo/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id, err := strconv.Atoi(chi.URLParam(r, "id"))
+
+		if err != nil {
+			fmt.Println("Could get valid id from path")
+			w.WriteHeader(400)
+			w.Write([]byte("invalid id"))
+		}
+
+		for i := range tableData.Todos {
+			if tableData.Todos[i].Id == id {
+				rand.Seed(time.Now().UnixNano())
+				n := 100 + rand.Intn(400)
+				time.Sleep(time.Duration(n) * time.Millisecond)
+
+				rowTmpl.Execute(w, tableData.Todos[i])
+				break
+			}
+		}
+	})
+
+	http.ListenAndServe(":1234", router)
+}
